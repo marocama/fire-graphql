028366699e799d3086f7081ba84ea9c82ce49319
marocama <marcus.rcm@outlook.com>
2023-11-04 04:28:14 -0300

    upload

---
+++ README.md
@@ -0,0 +1,5 @@
+# fire-graphql
+
+Aplicação base utilizando serviços do Google Firebase com o GraphQL.
+
+Utilizando `express` e `apollo-server-express`, rodando nas Functions e o Firestore como banco de dados.
---
+++ functions/index.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const admin = require('firebase-admin')
+const functions = require('firebase-functions')
+const { ApolloServer, gql } = require('apollo-server-express')
+
+!admin.apps.length ? admin.initializeApp() : admin.app()
+
+const app = express()
+const db = admin.firestore()
+
+
+const typeDefs = gql`
+  type User {
+    name: String
+    email: String
+  }
+
+  type Query {
+    users : [User]
+  }
+`
+
+const resolvers = {
+  Query: {
+    users: () => {
+      return new Promise((resolve, reject) => {
+        fetchAllUsers(data => resolve(data))
+      })
+    }
+  }
+}
+
+const fetchAllUsers = callback => {
+  db.collection('Users').get()
+  .then(result => callback(result.docs.map(i => ({ ...i.data() }))) )
+  .catch(e => console.log(e))
+}
+
+const server = new ApolloServer({ typeDefs, resolvers })
+
+server.start().then(res => {
+  server.applyMiddleware({ app, path: '/' })
+})
+
+exports.graphql = functions.region('southamerica-east1').https.onRequest(app)
